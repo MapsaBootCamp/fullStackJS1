58370894ab365f6784efd70906edb28e52550b34
teghfo <teghfo@gmail.com>
2023-01-20 19:54:26 +0330

    W1: cont. end of Array

--- Basic/W1/J2.js
+++ Basic/W1/J2.js
@@ -102,7 +102,6 @@ const myArr = ["Ashkan", 23, true, NaN, ["A", 12], { username: "Ashkan" }];
 //   console.log(indx);
 // }
 
-
 // console.log(myArr.map((val) => Array.isArray(val)));
 // console.log(
 //   myArr.map((val, indx, arr) => {
@@ -112,16 +111,36 @@ const myArr = ["Ashkan", 23, true, NaN, ["A", 12], { username: "Ashkan" }];
 //   })
 // );
 
+users = ["user-10", "user-2", "user-3"];
+
+// console.log(
+//   users.filter((item) => {
+//     const itemSplited = item.split("-");
+//     return parseInt(itemSplited[itemSplited.length - 1]) > 2;
+//   })
+// );
+
+// console.log(
+//   users.find((item) => {
+//     const itemSplited = item.split("-");
+//     return parseInt(itemSplited[itemSplited.length - 1]) > 2;
+//   })
+// );
+
+// users.forEach((val, indx) => {
+//   console.log(indx, val);
+// });
+
 ///////// functions in JS
 
 /// regular
 
-function regularFunc(arguments) {
-  //// block function
-}
+// function regularFunc(arguments) {
+//// block function
+// }
 
 /// arrow function
-const myArrowFunc = (arguments) => {};
+// const myArrowFunc = (arguments) => {};
 
 // const isPrime = (numb) => {
 //   for (let i = 2; i < numb; i++) {
@@ -132,3 +151,75 @@ const myArrowFunc = (arguments) => {};
 //   return true;
 // };
 // console.log(isPrime(13));
+
+///// TODO: func(arr, num) ====> return new arr
+
+const argArr = [3, -2, 32, 12];
+const argNum = 12;
+
+// console.log(findLowerThan(argArr, argNum));
+
+// function findLowerThan(arr, numb) {
+//   return arr.filter((val) => val < numb);
+// }
+
+// function findLowerThan(arr, numb) {
+//   const result = [];
+//   arr.forEach((val) => {
+//     if (val < numb) result.push(val);
+//   });
+//   return result;
+// }
+
+// console.log(findLowerThan(argArr, argNum));
+
+///// TODO: [2, 3, 3, 2, 1, 12] ====> [[2, 2], [3, 2], [1, 1], [12, 1]]
+
+const findFreq = function (arr) {
+  const checkedNumbers = [];
+  const result = [];
+
+  for (let elm of arr) {
+    if (checkedNumbers.includes(elm)) {
+      continue;
+    }
+    checkedNumbers.push(elm);
+    let freq = 0;
+    for (let item of arr.slice(arr.indexOf(elm))) {
+      if (item == elm) {
+        freq++;
+      }
+    }
+    result.push([elm, freq]);
+  }
+  return result;
+};
+
+function findFreq2(arr) {
+  const checkedNumbers = [];
+  const result = [];
+
+  for (let elm of arr) {
+    if (checkedNumbers.includes(elm)) {
+      continue;
+    }
+    checkedNumbers.push(elm);
+    let freqArr = arr.slice(arr.indexOf(elm)).filter((val) => val == elm);
+    result.push([elm, freqArr.length]);
+  }
+  return result;
+}
+
+// console.log(findFreq2([2, 3, 3, 2, 1, 12, 3]));
+
+users[6] = "Gholi";
+users["salam"] = 12;
+users[-2] = 8;
+users[1.2] = 12;
+console.log(users);
+console.log(users.length);
+for (const elm of users) {
+  console.log(elm);
+}
+
+console.log(users.indexOf(8));
