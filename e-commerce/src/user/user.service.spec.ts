1051a0486687a9dcf2e781e1e6e70dd42245af25
teghfo <teghfo@gmail.com>
2023-05-26 15:45:45 +0330

    W9-J2: Test Module in Nest...

--- e-commerce/src/user/user.service.spec.ts
+++ e-commerce/src/user/user.service.spec.ts
@@ -1,18 +1,52 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model, Promise } from 'mongoose';
+import { User } from './schemas/user.schema';
 import { UserService } from './user.service';
+
+const mockUser = {
+  email: 'user@test.com',
+  password: '1234',
+  phoneNumber: '09121231231',
+  role: 'CUSTOMER',
+  addresses: [],
+  active: true,
+};
 
 describe('UserService', () => {
   let service: UserService;
+  let model: Model<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockUser),
+            constructor: jest.fn().mockResolvedValue(mockUser),
+            exists: jest.fn(),
+            create: jest
+              .fn()
+              .mockImplementationOnce(() => Promise.resolve(mockUser)),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    model = module.get<Model<User>>(getModelToken('User'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create user service', async () => {
+    jest.spyOn(model, 'exists').mockResolvedValue(null);
+    const { email, password, phoneNumber } = mockUser;
+    const createdUser = await service.createUser(email, password, phoneNumber);
+    expect(createdUser).toEqual(mockUser);
   });
 });
